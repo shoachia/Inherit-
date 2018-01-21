b15e59efc1f446a6ac860b143bdcce55ae85eb64
shoachia <34368675+shoachia@users.noreply.github.com>
2018-01-21 22:44:40 +0800

    C++
    C++code for inherit implementation

---
+++ HourlyEmployee.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include "HourlyEmployee.h"
+
+HourlyEmployee::HourlyEmployee(const string &first,const string &last,const string &ssn,int month,int day,int year, double hourlyWage,double hoursWorked)
+:Employee(first,last,ssn,month,day,year)
+{
+	setWage(hourlyWage);
+	setHours(hoursWorked);
+}
+
+void HourlyEmployee::setWage(double hourlyWage)
+{
+	wage=(hourlyWage<0.0)?0.0:hourlyWage;
+}
+double HourlyEmployee::getWage() const
+{
+	return wage;
+}
+void HourlyEmployee::setHours(double hoursWorked)
+{
+	hours=(hoursWorked<0.0)?0.0:hoursWorked;
+}
+double HourlyEmployee::getHours() const
+{
+	return hours;
+} 
+double HourlyEmployee::earnings() const
+{
+	if(getHours()<=40)
+	return getWage()*getHours();
+	else
+	return 40*getWage()+((getHours()-40)*getWage()*1.5);
+}
+void HourlyEmployee::print() const
+{
+	cout<<"hourly employees: ";
+	Employee::print();
+	cout<<"\nhourly wage: "<<getWage()<<"; hours worked"<<getHours();
+}
+
